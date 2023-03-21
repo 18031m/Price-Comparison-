bc0f1e53b989cd3617fe18a73e6656291c7ed250
Markus Seibt <127067692+18031m@users.noreply.github.com>
2023-03-21 20:53:40 +0000

    added a simple gui template using chatgpt

--- main.py
+++ main.py
@@ -7,3 +7,22 @@
   TIP: Use assessment guide to help guide you through this Internal
 '''
 
+import tkinter as tk
+
+# Create the main window
+root = tk.Tk()
+root.title("My App")
+
+# Create the frame
+frame = tk.Frame(root, width=300, height=200)
+frame.pack()
+
+# Add some widgets to the frame
+label = tk.Label(frame, text="Hello, world!")
+label.pack()
+
+button = tk.Button(frame, text="Click me!")
+button.pack()
+
+# Run the main loop
+root.mainloop()
