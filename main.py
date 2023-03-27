2bf1e9fd2bf37fbf2959fd8bba9b8214ef69356a
Markus Seibt <127067692+18031m@users.noreply.github.com>
2023-03-27 01:41:21 +0000

    I added a list where users can input the price of the item.

--- main.py
+++ main.py
@@ -12,20 +12,26 @@ import tkinter as tk
 # Function to add items to the listbox
 def add_item():
     item = item_entry.get()
-    items_listbox.insert(tk.END, item)
+    price = price_entry.get()
+    items_listbox.insert(tk.END, f"{item}: ${price}")
     item_entry.delete(0, tk.END)
+    price_entry.delete(0, tk.END)
 
 # Create the main window
 root = tk.Tk()
 root.title("My Simple GUI")
 
-# Create a label
-my_label = tk.Label(root, text="Enter items:")
-my_label.pack()
-
-# Create an entry for the user to input items
+# Create a label for item name and entry field for item name
+item_label = tk.Label(root, text="Enter item name:")
+item_label.pack()
 item_entry = tk.Entry(root)
 item_entry.pack()
+
+# Create a label for item price and entry field for item price
+price_label = tk.Label(root, text="Enter item price:")
+price_label.pack()
+price_entry = tk.Entry(root)
+price_entry.pack()
 
 # Create a button to add items to the listbox
 add_button = tk.Button(root, text="Add", command=add_item)
