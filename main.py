37049857c7c22ea0998135e9adfc1f217f813a6e
Markus Seibt <127067692+18031m@users.noreply.github.com>
2023-03-27 01:39:55 +0000

    I added a list so users can input the name of the item or items.

--- main.py
+++ main.py
@@ -9,17 +9,31 @@
 
 import tkinter as tk
 
+# Function to add items to the listbox
+def add_item():
+    item = item_entry.get()
+    items_listbox.insert(tk.END, item)
+    item_entry.delete(0, tk.END)
+
 # Create the main window
 root = tk.Tk()
 root.title("My Simple GUI")
 
 # Create a label
-my_label = tk.Label(root, text="Hello, World!")
+my_label = tk.Label(root, text="Enter items:")
 my_label.pack()
 
-# Create a button
-my_button = tk.Button(root, text="Click me!")
-my_button.pack()
+# Create an entry for the user to input items
+item_entry = tk.Entry(root)
+item_entry.pack()
+
+# Create a button to add items to the listbox
+add_button = tk.Button(root, text="Add", command=add_item)
+add_button.pack()
+
+# Create a listbox to display the items
+items_listbox = tk.Listbox(root)
+items_listbox.pack()
 
 # Run the main loop
 root.mainloop()
