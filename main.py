a50a8d91ccc135e01dec178b3168feef42f8e67e
Markus Seibt <127067692+18031m@users.noreply.github.com>
2023-03-30 21:43:15 +0000

    I made it so the textbox that asks the user if they want to add a new item visible.

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import tkinter as tk
+import tkinter.messagebox
 
 # Function to add items to the listbox
 def add_item():
     item = item_entry.get()
     price = price_entry.get()
     quantity = quantity_entry.get()
-    items_listbox.insert(tk.END, f"{item} ({quantity}): ${price}")
+    items_listbox.insert(tk.END, f"{item} ({quantity}g): ${price}")
     item_entry.delete(0, tk.END)
     price_entry.delete(0, tk.END)
     quantity_entry.delete(0, tk.END)
     add_new_item()
 
-# Function to calculate the highest value item
+# Function to calculate the highest value item in grams
 def calculate_highest_value():
     highest_value_item = ""
     highest_value = 0
@@ -19,12 +20,12 @@ def calculate_highest_value():
         item_info = item.split(":")
         item_price = float(item_info[1][1:])
         item_quantity = int(item_info[0].split("(")[-1][:-1])
-        item_total_value = item_price * item_quantity
-        if item_total_value > highest_value:
-            highest_value = item_total_value
+        item_total_value_grams = item_price * item_quantity * 28.35 # convert ounces to grams
+        if item_total_value_grams > highest_value:
+            highest_value = item_total_value_grams
             highest_value_item = item
     highest_value_textbox.delete("1.0", tk.END)
-    highest_value_textbox.insert(tk.END, f"The highest value item is {highest_value_item} with a total value of ${highest_value}")
+    highest_value_textbox.insert(tk.END, f"The highest value item is {highest_value_item} with a total value of {highest_value:.2f} grams")
 
 # Function to add a new item
 def add_new_item():
@@ -40,7 +41,7 @@ def add_new_item():
 # Create the main window
 root = tk.Tk()
 root.title("My Simple GUI")
-root.geometry("400x400")
+root.geometry("400x420")
 
 # Create a label for budget and entry field for budget
 budget_label = tk.Label(root, text="Enter your budget:")
@@ -61,7 +62,7 @@ price_entry = tk.Entry(root, width=30)
 price_entry.pack()
 
 # Create a label for item quantity and entry field for item quantity
-quantity_label = tk.Label(root, text="Enter item quantity:")
+quantity_label = tk.Label(root, text="Enter item quantity (grams):")
 quantity_label.pack()
 quantity_entry = tk.Entry(root, width=30)
 quantity_entry.pack()
@@ -71,16 +72,8 @@ add_button = tk.Button(root, text="Add", command=add_item)
 add_button.pack()
 
 # Create a listbox to display the items
-items_listbox = tk.Listbox(root, width=50)
+items_listbox = tk.Listbox(root, width=30)
 items_listbox.pack()
 
 # Create a button to calculate the highest value item
-highest_value_button = tk.Button(root, text="Calculate Highest Value Item", command=calculate_highest_value)
-highest_value_button.pack()
-
-# Create a text box to display the highest value item
-highest_value_textbox = tk.Text(root, width=50, height=3)
-highest_value_textbox.pack()
-
-# Run the main loop
-root.mainloop()
+highest_value_button = tk.Button
