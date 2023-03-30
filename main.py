d0f104ba55fc8704147b1e42eae15a75238a9664
Markus Seibt <127067692+18031m@users.noreply.github.com>
2023-03-30 21:06:23 +0000

    I expanded the text boxes to all be equal in width.

--- main.py
+++ main.py
@@ -20,6 +20,21 @@ def add_item():
     quantity_entry.delete(0, tk.END)
     add_new_item()
 
+# Function to calculate the highest value item
+def calculate_highest_value():
+    highest_value_item = ""
+    highest_value = 0
+    for item in items_listbox.get(0, tk.END):
+        item_info = item.split(":")
+        item_price = float(item_info[1][1:])
+        item_quantity = int(item_info[0].split("(")[-1][:-1])
+        item_total_value = item_price * item_quantity
+        if item_total_value > highest_value:
+            highest_value = item_total_value
+            highest_value_item = item
+    highest_value_textbox.delete("1.0", tk.END)
+    highest_value_textbox.insert(tk.END, f"The highest value item is {highest_value_item} with a total value of ${highest_value}")
+
 # Function to add a new item
 def add_new_item():
     new_item = tk.messagebox.askyesno("Add New Item", "Do you want to add a new item?")
@@ -34,30 +49,30 @@ def add_new_item():
 # Create the main window
 root = tk.Tk()
 root.title("My Simple GUI")
-root.geometry("400x400") # Set the width to 400 pixels and height to 300 pixels
+root.geometry("400x420")
 
 # Create a label for budget and entry field for budget
 budget_label = tk.Label(root, text="Enter your budget:")
 budget_label.pack()
-budget_entry = tk.Entry(root)
+budget_entry = tk.Entry(root, width=30)
 budget_entry.pack()
 
 # Create a label for item name and entry field for item name
 item_label = tk.Label(root, text="Enter item name:")
 item_label.pack()
-item_entry = tk.Entry(root)
+item_entry = tk.Entry(root, width=30)
 item_entry.pack()
 
 # Create a label for item price and entry field for item price
 price_label = tk.Label(root, text="Enter item price:")
 price_label.pack()
-price_entry = tk.Entry(root)
+price_entry = tk.Entry(root, width=30)
 price_entry.pack()
 
 # Create a label for item quantity and entry field for item quantity
 quantity_label = tk.Label(root, text="Enter item quantity:")
 quantity_label.pack()
-quantity_entry = tk.Entry(root)
+quantity_entry = tk.Entry(root, width=30)
 quantity_entry.pack()
 
 # Create a button to add items to the listbox
@@ -65,8 +80,16 @@ add_button = tk.Button(root, text="Add", command=add_item)
 add_button.pack()
 
 # Create a listbox to display the items
-items_listbox = tk.Listbox(root)
+items_listbox = tk.Listbox(root, width=30)
 items_listbox.pack()
+
+# Create a button to calculate the highest value item
+highest_value_button = tk.Button(root, text="Calculate Highest Value Item", command=calculate_highest_value)
+highest_value_button.pack()
+
+# Create a text box to display the highest value item
+highest_value_textbox = tk.Text(root, width=34, height=2)
+highest_value_textbox.pack()
 
 # Run the main loop
 root.mainloop()
